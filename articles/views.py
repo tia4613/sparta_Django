7442372ae5702014d4f422d9ba917ea38ecb3f66
tia <duqrl3000@gmail.com>
2024-04-17 22:06:25 +0900

    변수선언

--- articles/views.py
+++ articles/views.py
@@ -4,9 +4,12 @@ from django.shortcuts import render
 def index(request):
 	return render(request, "index.html")
 
-
 def users(request):
 	return render(request, "users.html")
 
 def hello(request):
-	return render(request, "hello.html")
+	# 변수 선언 - 이름을 바꿔준다
+	context = { 
+		"name":"hwan",
+	}
+	return render(request, "hello.html",context)
