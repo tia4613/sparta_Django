5d60082476a5e84f06325bc22f4197bab813d8ea
tia <duqrl3000@gmail.com>
2024-04-18 15:04:21 +0900

    data_catch

--- articles/views.py
+++ articles/views.py
@@ -21,3 +21,6 @@ def hello(request):
 
 def data_throw(request):
   return render(request, "data_throw.html")
+
+def data_catch(request):
+  return render(request, "data_catch.html")  
--- my_first_pjt/urls.py
+++ my_first_pjt/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('hello/', views.hello),
 
     path('data_throw/', views.data_throw),
+    path('data_catch/', views.data_catch),
 
 ]
