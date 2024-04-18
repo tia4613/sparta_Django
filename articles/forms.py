76093ecd8f56c2a9f92881db0e448647e887a23c
tia <duqrl3000@gmail.com>
2024-04-19 00:50:30 +0900

    모델 폼 사용

--- articles/forms.py
+++ articles/forms.py
@@ -1,14 +1,10 @@
 from django import forms
 
+from articles.models import Article
 
-class ArticleForm(forms.Form):
-    # 앞은 데이터베이스에 저장될 값, 뒤는 사용자에게 보여질 값
-    GENRE_CHOICES = [
-        ("technology", "Technology"),
-        ("life", "Life"),
-        ("hobby", "Hobby"),
-    ]
 
-    title = forms.CharField(max_length=10)
-    content = forms.CharField(widget=forms.Textarea)
-    genre = forms.ChoiceField(choices=GENRE_CHOICES)
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = "__all__" # 모든 필드를 다 가져와서 모델로 만들어줘
+        # exclude = ["title"] # 대신 그 중에 타이틀만 빼주고 (제외)
