e6ddef506127d7b6463e5b60551de32756cd8e85
tia <duqrl3000@gmail.com>
2024-04-19 01:16:34 +0900

    edit을 update로 바꾸기

--- articles/views.py
+++ articles/views.py
@@ -25,24 +25,23 @@ def create(request):
   else:
       form = ArticleForm()
 
-  context = {"forms": forms}
+  context = {"form": form,}
   return render(request, "create.html", context)
 
-def edit(request, pk):
-	article = Article.objects.get(pk=pk)
-	context = { "article": article,}
-	return render(request, "edit.html", context)
-
 def update(request, pk):
-  title = request.POST.get("title")
-  content = request.POST.get("content")
-  
-  article = Article.objects.get(pk=pk)
-  article.title = title
-  article.content = content
-  article.save()
-
-  return redirect("detail", article.id)
+    article = Article.objects.get(pk=pk)
+    if request.method == "POST":
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            article = form.save()
+            return redirect("article_detail", article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    context = {
+        "form": form,
+        "article": article,
+    }
+    return render(request, "update.html", context)
 
 def delete(request, pk):
   article = Article.objects.get(pk=pk)
