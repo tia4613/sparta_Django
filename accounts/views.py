84a4b63a48c242a92e72a04b184fa8def7f45e21
tia <duqrl3000@gmail.com>
2024-04-19 02:46:53 +0900

    로그아웃 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 
 def login(request):
@@ -7,8 +9,12 @@ def login(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
-            return redirect("index ")
+            return redirect("index")
     else:
         form = AuthenticationForm()
     context = {"form": form}
     return render(request, "accounts/login.html", context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect("index")
