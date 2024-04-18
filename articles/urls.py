3808c29fa30be96aa28a30b5879a4bf3666c487e
tia <duqrl3000@gmail.com>
2024-04-18 23:52:39 +0900

    수정하기

--- articles/urls.py
+++ articles/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path("create/", views.create, name="create"),
     path("<int:pk>/", views.detail, name="detail"),
     path("<int:pk>/delete/", views.delete, name="delete"),
+    path("<int:pk>/edit", views.edit, name="edit"),
+    path("<int:pk>/update/", views.update, name="update"),
 
     path('data_throw/', views.data_throw, name='data_throw'),
     path('data_catch/', views.data_catch, name='data_catch'),
--- articles/views.py
+++ articles/views.py
@@ -30,6 +30,22 @@ def create(request):
     article.save()
     return redirect("detail", article.id)
 
+def edit(request, pk):
+	article = Article.objects.get(pk=pk)
+	context = { "article": article,}
+	return render(request, "edit.html", context)
+
+def update(request, pk):
+  title = request.POST.get("title")
+  content = request.POST.get("content")
+  
+  article = Article.objects.get(pk=pk)
+  article.title = title
+  article.content = content
+  article.save()
+
+  return redirect("detail", article.id)
+
 def delete(request, pk):
   article = Article.objects.get(pk=pk)
   if request.method == "POST":
