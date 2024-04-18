cf87b54be196979479e913a7f8e49ef3d8a3a71e
tia <duqrl3000@gmail.com>
2024-04-18 19:36:45 +0900

    마이그래이션 추가 등록

--- articles/models.py
+++ articles/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Article(models.Model):
   title = models.CharField(max_length=50)
   content = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True) #최초추가
+  updated_at = models.DateTimeField(auto_now=True) #수정될때마다
