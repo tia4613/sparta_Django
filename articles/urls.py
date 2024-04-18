3ab72c187c825b65733864614279d7f871ae5a90
tia <duqrl3000@gmail.com>
2024-04-18 21:08:25 +0900

    new와 create

--- articles/urls.py
+++ articles/urls.py
@@ -4,6 +4,9 @@ from . import views #.은 내 위치
 urlpatterns = [
 
     path('', views.articles, name='articles'),
+    path("new/", views.new, name="new"),
+    path("create/", views.create, name="create"),
+    
     path('data_throw/', views.data_throw, name='data_throw'),
     path('data_catch/', views.data_catch, name='data_catch'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -12,6 +12,22 @@ def articles(request):
     }
   return render(request, "articles.html", context)
 
+def new(request):
+    return render(request, "new.html")
+
+def create(request):
+    # GET 방식으로 전달된 데이터를 받아서
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+
+    # 받은 데이터를 Article 모델을 이용해서 저장
+    article = Article(title=title, content=content)
+    article.save()
+    context = {
+        "article": article,
+    }
+    return render(request, "create.html", context)
+
 def data_throw(request):
   return render(request, "data_throw.html")
 
