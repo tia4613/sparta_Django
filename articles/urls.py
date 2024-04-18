74df54ce3caabc325ea279c71422e51098d556c7
tia <duqrl3000@gmail.com>
2024-04-18 18:15:37 +0900

    throw,catch 경로 바꾸어주기

--- articles/urls.py
+++ articles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views #.은 내 위치
 
 urlpatterns = [
-    path('hello/', views.hello),
-    path('data_throw/', views.data_throw),
-    path('data_catch/', views.data_catch),
+    path('hello/', views.hello, name='hello'),
+    path('data_throw/', views.data_throw, name='data_throw'),
+    path('data_catch/', views.data_catch, name='data_catch'),
 ]
