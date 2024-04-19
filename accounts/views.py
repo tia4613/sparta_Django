5ff90e2613f791a3f963992484238feb45ec48d6
tia <duqrl3000@gmail.com>
2024-04-19 11:23:15 +0900

    회원가입기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.views.decorators.http import require_POST, require_http_methods
+from django.contrib.auth.forms import UserCreationForm
+
 
 @require_http_methods(["GET", "POST"])
 def login(request):
@@ -22,3 +24,14 @@ def logout(request):
   if request.user.is_authenticated:
     auth_logout(request)
   return redirect("index")
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    else:
+        form = UserCreationForm()
+    context = {"form": form}
+    return render(request, "accounts/signup.html", context)
